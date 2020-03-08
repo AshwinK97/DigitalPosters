63b182db6c056ab7e05489828b6bd2cace365e11
AshwinK97 <ashwin.kamalakannan@ontariotechu.net>
2020-03-08 19:06:49 -0400

    restructured server.js

--- server/server.js
+++ server/server.js
@@ -1,30 +1,24 @@
-// import data from "posterJSON";
 const express = require("express");
-const app = express();
 const objectID = require("mongodb").ObjectID;
-const router = express.Router();
-
-const Port = process.env.Port || 3000;
-
 const MongoClient = require("mongodb").MongoClient;
-
-const uri = "mongodb+srv://jasUser:jasUser@cluster0-ewygn.mongodb.net/test?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri, { useNewUrlParser: true });
-
-// console.log(JSON.stringify(data));
-
 const fs = require("fs");
 
-let rawdata = fs.readFileSync("posterJSON.json");
-let posterInfo = JSON.parse(rawdata);
-console.log(posterInfo);
+const app = express();
 
-client.connect(err => {
-  const db = client.db("eposter");
-  var collection = db.collection("posters");
-  // perform actions on the collection object
-  app.listen(Port, () => console.log("Server started"));
+const uri = "mongodb+srv://jasUser:jasUser@cluster0-ewygn.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+app.get("/readjson", (req, res) => {
+  let rawdata = fs.readFileSync("posterJSON.json");
+  let posterInfo = JSON.parse(rawdata);
+
+  // client.connect(err => {
+  //   const db = client.db("eposter");
+  //   var collection = db.collection("posters");
+  //   collection.find({userID: posterInfo.})
+  // });
+
+  res.send(posterInfo);
 });
 
 // collection.find({ userID: userID, posterID: posterID }).toArray((error, results) => {
@@ -47,3 +41,5 @@ client.connect(err => {
 //     );
 //   }
 // });
+
+app.listen(3000, () => console.log(`Server listening on port: 3000`));
