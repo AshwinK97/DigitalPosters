96e2d2946e770dc4dbc77743903a22d1b8ba1546
DarronSingh <darron.singh@uoit.net>
2020-03-09 22:56:13 -0400

    authentication, front end

---
+++ vue-auth/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    computed : {
+      isLoggedIn : function(){ return this.$store.getters.isLoggedIn}
+    },
+    methods: {
+      logout: function () {
+        this.$store.dispatch('logout')
+        .then(() => {
+          this.$router.push('/login')
+        })
+      }
+    },
+    created: function () {
+    this.$http.interceptors.response.use(undefined, function (err) {
+      return new Promise(function () {
+        if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+          this.$store.dispatch(this.logout)
+        }
+        throw err;
+      });
+    });
+  }
+
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  cursor: pointer;
+}
+#nav a:hover {
+  text-decoration: underline;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
